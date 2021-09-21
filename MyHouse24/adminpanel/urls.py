2d1524ef7033b824cbc1a88305d2d51670b2fd4e
Nikita(WorkLaptopLin) <falgerok.master@gmail.com>
2021-09-21 16:55:06 +0300

    Добавил слайдер и начал делать обновление форм

--- MyHouse24/adminpanel/urls.py
+++ MyHouse24/adminpanel/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('house/', views.house, name='house'),
     path('house/create', views.create_house, name='create_house'),
     path('house/<int:id>', views.info_house, name='info_house'),
+    path('house/update/<int:id>', views.update_house, name='update_house'),
     # Авторизация
     path('login/', views.login_admin, name='login_admin'),
     path('logout/', views.logout_admin, name='logout_admin'),
--- MyHouse24/adminpanel/views.py
+++ MyHouse24/adminpanel/views.py
@@ -64,6 +64,7 @@ def create_house(request):
             if form_section.is_valid():
                 for subform in form_section:
                     if not subform.cleaned_data['DELETE']:
+                        print(subform.cleaned_data)
                         obj = subform.save(commit=False)
                         obj.house = form.save(commit=False)
                         obj.save()
@@ -88,6 +89,82 @@ def create_house(request):
         'FloorFormSet': FloorFormSet(prefix='floor'),
     }
     return render(request, "adminpanel/house/create.html", data)
+
+def update_house(request, id):
+    house_data = House.objects.get(id=id)
+    sections = Section.objects.filter(house=id)
+    floors = Floor.objects.filter(house=id)
+
+    SectionFormSet = formset_factory(SectionForm, extra=0, can_delete=True)
+    FloorFormSet = formset_factory(FloorForm, extra=0, can_delete=True)
+
+    if request.method == 'POST':
+        form_section = SectionFormSet(request.POST, prefix='section')
+        form_floor = FloorFormSet(request.POST, prefix='floor')
+
+        name = request.POST.get('name')
+        address = request.POST.get('address')
+        image_1 = request.POST.get('image_1')
+        image_2 = request.POST.get('image_2')
+        image_3 = request.POST.get('image_3')
+        image_4 = request.POST.get('image_4')
+        image_5 = request.POST.get('image_5')
+
+        House.objects.filter(id=id).update(
+            name = name,
+            address = address,
+            image_1 = image_1,
+            image_2 = image_2,
+            image_3 = image_3,
+            image_4 = image_4,
+            image_5 = image_5,
+            )
+
+        if form_section.is_valid():
+            for subform in form_section:
+                if not subform.cleaned_data['DELETE']:
+                    obj = subform.save(commit=False)
+                    obj.house = house_data
+                    obj.save()
+        else:
+            print(form_section.errors)
+        # Сохранение этажей
+        if form_floor.is_valid():
+            for subform in form_floor:
+                if not subform.cleaned_data['DELETE']:
+                    obj = subform.save(commit=False)
+                    obj.house = house_data
+                    obj.save()
+        else:
+            print(form_floor.errors)
+        return redirect('house')
+
+    house_form = HouseForm(initial={
+        'name': house_data.name,
+        'address': house_data.address,
+        'image_1': house_data.image_1,
+        'image_2': house_data.image_2,
+        'image_3': house_data.image_3,
+        'image_4': house_data.image_4,
+        'image_5': house_data.image_5,
+    })
+
+    # section_list = []
+    # for section in sections:
+    #     section_list.append(section)
+    #
+    # floor_list = []
+    # for floor in floors:
+    #     floor_list.append(floor)
+
+
+    data = {
+        'house': house_form,
+        # 'sections': SectionFormSet(section_list),
+        # 'floors': FloorFormSet(floor_list),
+    }
+    return render(request, 'adminpanel/house/update.html', data)
+
 
 def info_house(request, id):
     house = House.objects.get(id=id)
