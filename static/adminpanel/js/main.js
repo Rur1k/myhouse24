1e6039b266dd15b340cb336fea9d13a7394f3dc0
Nikita(WorkLaptopLin) <falgerok.master@gmail.com>
2021-11-30 14:48:07 +0200

    Сделал фильры для полей input

--- static/adminpanel/js/main.js
+++ static/adminpanel/js/main.js
@@ -10,70 +10,13 @@ $("document").ready(function() {
 
     MultiplicationInvoice()
     // Инициализация DataTable
-    FilterInput('#AccountTransactionTable', [2])
-
-
-/*    $('#AccountTransactionTable thead tr')
-        .clone(true)
-        .addClass('filters')
-        .appendTo('#AccountTransactionTable thead');
-
-    var table = $('#AccountTransactionTable').DataTable({
-        dom: 't',
-        ordering: false,
-        paging: false,
-        orderCellsTop: true,
-        fixedHeader: true,
-        initComplete: function () {
-            var api = this.api();
-            // For each column
-            api
-                .column([0])
-                .eq(0)
-                .each(function (colIdx) {
-                    // Set the header cell to contain the input element
-                    var cell = $('.filters th').eq(
-                        $(api.column(colIdx).header()).index()
-                    );
-                    var title = $(cell).text();
-                    $(cell).html('<input type="text">');
-                    // On every keypress in this input
-                    $(
-                        'input',
-                        $('.filters th').eq($(api.column(colIdx).header()).index())
-                    )
-                        .off('keyup change')
-                        .on('keyup change', function (e) {
-                            e.stopPropagation();
-
-                            // Get the search value
-                            $(this).attr('title', $(this).val());
-                            var regexr = '({search})'; //$(this).parents('th').find('select').val();
-
-                            var cursorPosition = this.selectionStart;
-                            // Search the column for that value
-                            api
-                                .column(colIdx)
-                                .search(
-                                    this.value != ''
-                                        ? regexr.replace('{search}', '(((' + this.value + ')))')
-                                        : '',
-                                    this.value != '',
-                                    this.value == ''
-                                )
-                                .draw();
-                            $(this)
-                                .focus()[0]
-                                .setSelectionRange(cursorPosition, cursorPosition);
-                        });
-                });
-        },
-    });*/
+    FilterInput('#AccountTransactionTable', [0,1,5], [2,3,4,6,7,8])
 
 
 });
 
-function FilterInput(Table, arr) {
+//Функция фильтрации input
+function FilterInput(Table, arr, empty) {
     $(Table+' thead tr')
         .clone(true)
         .addClass('filters')
@@ -87,9 +30,20 @@ function FilterInput(Table, arr) {
         fixedHeader: true,
         initComplete: function () {
             var api = this.api();
+
+            // Заполнение полей путыми значениями
+            api.columns(empty).eq(0).each(function (colIdx) {
+                    // Set the header cell to contain the input element
+                    var cell = $('.filters th').eq(
+                        $(api.column(colIdx).header()).index()
+                    );
+                    var title = $(cell).text();
+                    $(cell).html('<text></text>');
+            });
+
             // For each column
             api
-                .column(arr)
+                .columns(arr)
                 .eq(0)
                 .each(function (colIdx) {
                     // Set the header cell to contain the input element
